d3efeb8d4b67cddf0fa67b6d59df7d6a343b6deb
Dipankar Majumder <dipankarmajumder2000@outlook.com>
2019-12-30 18:31:03 +0530

    Updates

---
+++ precache-manifest.ac3473035ac72373288f281040ce317e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe9e11e9f760599f6033a0d5558bf3d",
+    "url": "/exam-management-react/index.html"
+  },
+  {
+    "revision": "5e6203f3af9e351ef6a8",
+    "url": "/exam-management-react/static/css/main.efbb4259.chunk.css"
+  },
+  {
+    "revision": "7f7f1681ca2395e0608e",
+    "url": "/exam-management-react/static/js/2.fe2b4170.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/exam-management-react/static/js/2.fe2b4170.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e6203f3af9e351ef6a8",
+    "url": "/exam-management-react/static/js/main.2382be66.chunk.js"
+  },
+  {
+    "revision": "cc755c9c386f517d6898",
+    "url": "/exam-management-react/static/js/runtime-main.ac0debf3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/exam-management/precache-manifest.597b9532a798aa6f4fe638f480e1d103.js"
+  "/exam-management-react/precache-manifest.ac3473035ac72373288f281040ce317e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exam-management/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exam-management-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
