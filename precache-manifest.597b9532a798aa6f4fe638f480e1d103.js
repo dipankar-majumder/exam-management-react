ae89c977c610f37343aa90f53a668928a51ed5d7
Dipankar Majumder <dipankarmajumder2000@outlook.com>
2019-12-30 18:19:29 +0530

    Updates

---
+++ precache-manifest.597b9532a798aa6f4fe638f480e1d103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70bfc79ea2a74e6c740349f02e43ecb",
+    "url": "/exam-management/index.html"
+  },
+  {
+    "revision": "7bcf69a6d6e8540376aa",
+    "url": "/exam-management/static/css/main.efbb4259.chunk.css"
+  },
+  {
+    "revision": "7f7f1681ca2395e0608e",
+    "url": "/exam-management/static/js/2.fe2b4170.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/exam-management/static/js/2.fe2b4170.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bcf69a6d6e8540376aa",
+    "url": "/exam-management/static/js/main.b0980a26.chunk.js"
+  },
+  {
+    "revision": "996efa5a1145f4d30220",
+    "url": "/exam-management/static/js/runtime-main.5ebbd34f.js"
+  }
+]);
