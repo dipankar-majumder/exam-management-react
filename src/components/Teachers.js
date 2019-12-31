1bfac4c25d7b5f6f7431e683a3c762a810aa9daf
Dipankar Majumder <dipankarmajumder2000@outlook.com>
2019-12-31 20:24:56 +0530

    Enhanced Table Added

--- src/components/Teachers.js
+++ src/components/Teachers.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,6 +11,26 @@ import Paper from '@material-ui/core/Paper';
 
 import { changeAppBarTitle } from '../store/actions/stateActions';
 import { setTeachersLoading } from '../store/actions/teacherActions';
+
+import EnhancedTable from './EnhancedTable';
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.paper,
+    },
+  },
+}))(TableRow);
 
 const useStyles = makeStyles({
   root: {
@@ -23,8 +43,6 @@ const useStyles = makeStyles({
 });
 
 const Teachers = () => {
-  const classes = useStyles();
-
   const { teachers, isLoading } = useSelector(state => state.teacherReducer);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -32,30 +50,33 @@ const Teachers = () => {
     // dispatch(setTeachersLoading());
   }, []);
   useEffect(() => console.log('[log]'));
+
+  const classes = useStyles();
   return (
     <div>
-      <h1>Teachers</h1>
-      {JSON.stringify(teachers)}
-      <Paper className={classes.root}>
-        <Table className={classes.table} aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell align='right'>ID</TableCell>
-              <TableCell align='left'>Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {teachers.map(teacher => (
-              <TableRow key={teacher.id}>
-                <TableCell align='right' component='th' scope='row'>
-                  {teacher.id}
-                </TableCell>
-                <TableCell>{teacher.email}</TableCell>
+      <EnhancedTable tableTitle='Teachers' teachers={teachers} />
+      {/* <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align='right'>ID</StyledTableCell>
+                <StyledTableCell align='left'>Email</StyledTableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+            <TableBody>
+              {teachers.map(teacher => (
+                <StyledTableRow key={teacher.id}>
+                  <StyledTableCell align='right' component='th' scope='row'>
+                    {teacher.id}
+                  </StyledTableCell>
+                  <StyledTableCell>{teacher.email}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </Paper> */}
     </div>
   );
 };
